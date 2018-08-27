7cdde2124b8b2051fc222c8a7d6f6b4af10c4889
Johnny <jerh56@gmail.com>
2018-08-26 21:30:44 -0600

    se ajusto helmet

--- src/config/express.js
+++ src/config/express.js
@@ -76,11 +76,13 @@ function initMiddleware(app) {
  */
 function initHelmetHeaders(app) {
     // Use helmet to secure Express headers
-    app.use(helmet.xframe());
+    //app.use(helmet.xframe());
+    app.use(helmet.frameguard());
     app.use(helmet.xssFilter());
     app.use(helmet.nosniff());
     app.use(helmet.ienoopen());
     app.disable('x-powered-by');
+
 }
 
 /**
